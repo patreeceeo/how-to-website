19481db8de6b9528b26b16213a144b871acfb04c
Patrick Canfield <pscale01@gmail.com>
2022-12-27 16:03:44 -0800

    feat[fileTool]: fix whitespace

--- src/tools/fileTool.js
+++ src/tools/fileTool.js
@@ -6,7 +6,13 @@ function exportHtml() {
   for(const el of excludedElements) {
     el.parentNode.removeChild(el)
   }
-  return `<!DOCTYPE html>\n<html lang="en">\n  ${documentCopy.documentElement.innerHTML}</html>`
+  // This is complicated because the current version of Chromium doesn't perfectly preserve whitespace when retreiving HTML strings from the DOM
+  const bodyAttributes = documentCopy.body.attributes
+  let bodyAttributeStrings = []
+  for(const attr of bodyAttributes) {
+    bodyAttributeStrings.push(`${attr.name}="${attr.value}"`)
+  }
+  return `<!DOCTYPE html>\n<html lang="en">\n  ${documentCopy.head.outerHTML.trim()}\n  <body ${bodyAttributeStrings.join(' ')}>\n    ${documentCopy.body.innerHTML.trim()}\n  </body>\n</html>`
 }
 
 window.fileTool = {
